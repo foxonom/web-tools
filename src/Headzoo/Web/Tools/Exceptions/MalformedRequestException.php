da079d518ee337647d186fb151b7d04f4bdfec8c
headzoo <sean@headzoo.io>
2014-03-24 15:32:19 -0400

    Initial import

---
+++ src/Headzoo/Web/Tools/Exceptions/MalformedRequestException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Web\Tools\Exceptions;
+
+/**
+ * Thrown by http request parsers when the request is not formatted correctly.
+ */
+class MalformedRequestException
+    extends Exception {}
