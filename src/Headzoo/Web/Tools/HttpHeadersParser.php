7311e2817fa806ac7be37a6957da30ac621c079f
headzoo <sean@headzoo.io>
2014-03-24 15:13:11 -0400

    Implements HttpHeadersParserInterface

--- src/Headzoo/Web/Tools/HttpHeadersParser.php
+++ src/Headzoo/Web/Tools/HttpHeadersParser.php
@@ -5,18 +5,10 @@ namespace Headzoo\Web\Tools;
  * Parses request/response headers into an array.
  */
 class HttpHeadersParser
+    implements HttpHeadersParserInterface
 {
     /**
-     * Parses raw http headers, and returns them as an array
-     * 
-     * The $options argument will be populated with the request/response portion of the headers, eg "GET / HTTP/1.1"
-     * or "HTTP/1.1 200 OK" if found.
-     * 
-     * The header keys are normalized to ensure they are always formatted "Camel-Case", eg "Content-Type".
-     * 
-     * @param  string $headers The raw http headers
-     * @param  null   $options Populated with http options
-     * @return array
+     * {@inheritDoc}
      */
     public function parse($headers, &$options = null)
     {
