592089beb608911a08527a032a34fb933a22a2c4
headzoo <sean@headzoo.io>
2014-03-25 04:06:29 -0400

    Test index.php echos back request headers

--- tests/index.php
+++ tests/index.php
@@ -3,6 +3,16 @@
  * Web page for unit testing. This page echos back the $_SERVER, $_GET, and $_POST values as a json encoded
  * string.
  */
+header("X-Test-Response: Howdy");
+foreach($_SERVER as $key => $value) {
+    if (substr($key, 0, 5) == "HTTP_") {
+        $key = str_replace("HTTP_", "", $key);
+        $key = str_replace("_", " ", $key);
+        $key = ucwords(strtolower($key));
+        $key = str_replace(" ", "-", $key);
+        header("{$key}: {$value}");
+    }
+}
 $_SERVER["GET"]  = $_GET;
 $_SERVER["POST"] = $_POST;
 echo json_encode($_SERVER, JSON_PRETTY_PRINT);
