0d1c9a8a35c96cc647506145364d0ec67eda6947
headzoo <sean@headzoo.io>
2014-03-25 05:10:21 -0400

    Updated test

--- tests/index.php
+++ tests/index.php
@@ -3,16 +3,7 @@
  * Web page for unit testing. This page echos back the $_SERVER, $_GET, and $_POST values as a json encoded
  * string.
  */
-header("X-Test-Response: Howdy");
-foreach($_SERVER as $key => $value) {
-    if (substr($key, 0, 5) == "HTTP_") {
-        $key = str_replace("HTTP_", "", $key);
-        $key = str_replace("_", " ", $key);
-        $key = ucwords(strtolower($key));
-        $key = str_replace(" ", "-", $key);
-        header("{$key}: {$value}");
-    }
-}
+header("X-Testing-Pong: " . $_SERVER["HTTP_X_TESTING_PING"]);
 $_SERVER["GET"]  = $_GET;
 $_SERVER["POST"] = $_POST;
 echo json_encode($_SERVER, JSON_PRETTY_PRINT);
