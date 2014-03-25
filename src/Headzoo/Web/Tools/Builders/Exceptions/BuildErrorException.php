e94501e0ed74bb3bed5c0e50c38fc6cd7a3db13e
headzoo <sean@headzoo.io>
2014-03-24 22:38:33 -0400

    Initial import

---
+++ src/Headzoo/Web/Tools/Builders/Exceptions/BuildErrorException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Headzoo\Web\Tools\Builders\Exceptions;
+use Headzoo\Web\Tools\Exceptions\WebException;
+
+/**
+ * Thrown when a builder encounters a problem.
+ */
+class BuildErrorException
+    extends WebException {}
