464bbac6ab6519d5eff8104e5ffd21dc4e2f95bb
headzoo <sean@headzoo.io>
2014-03-24 22:45:20 -0400

    Initial import

---
+++ src/Headzoo/Web/Tools/Exceptions/RuntimeException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Web\Tools\Exceptions;
+
+/**
+ * Thrown if an error which can only be found on runtime occurs.
+ */
+class RuntimeException
+    extends \RuntimeException {}
